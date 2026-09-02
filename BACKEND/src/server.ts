c5455fa560bd6a4eb99bc5fe272653ff0aba6e7f
VictoriaEstrugo <victoriaestrugo2011@gmail.com>
2026-09-02 13:21:10 -0300

    cambios

---
+++ BACKEND/src/server.ts
@@ -0,0 +1,17 @@
+import express from "express";
+
+const app = express();
+
+const PORT = 3000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.json({
+        mensaje: "Backend MESH funcionando"
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor funcionando en http://localhost:${PORT}`);
+});
